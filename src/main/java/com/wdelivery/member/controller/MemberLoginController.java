93c4f9ade201ecf351185659828e21815ad74d4b
NIS <nes0510@gmail.com>
2021-10-31 22:12:27 +0900

    kakaotalk json to MemberLoginController/kakaoLogin.do

--- src/main/java/com/wdelivery/member/controller/MemberLoginController.java
+++ src/main/java/com/wdelivery/member/controller/MemberLoginController.java
@@ -3,8 +3,12 @@ package com.wdelivery.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wdelivery.member.service.MemberService;
+import com.wdelivery.member.vo.KakaoUserVO;
 import com.wdelivery.member.vo.UserVO;
 
 @Controller
@@ -16,10 +20,18 @@ public class MemberLoginController {
 	@Autowired
 	private MemberService memberService; 
 	
-	@PostMapping("/memLogin.do")
-	public void memberLogin(UserVO userVO) {
+	@PostMapping("memLogin.do")
+	public String memberLogin(UserVO userVO) {
 		UserVO findUserVO = memberService.searchID(userVO);
 				System.out.println(findUserVO.toString());
+		return "main";
+	}
+	
+	@RequestMapping("kakaoLogin.do")
+	@ResponseBody
+	public String kakaoLogin(@RequestBody KakaoUserVO kakaoVO) {
+		System.out.println(kakaoVO.toString());
+		return "main";
 	}
 	
 	
