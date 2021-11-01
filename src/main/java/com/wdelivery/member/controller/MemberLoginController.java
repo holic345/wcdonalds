6afcf01800525113c24ecbdf6844e9e49b3a8058
NIS <nes0510@gmail.com>
2021-11-01 15:14:21 +0900

    loginController, memberController edit

--- src/main/java/com/wdelivery/member/controller/MemberLoginController.java
+++ src/main/java/com/wdelivery/member/controller/MemberLoginController.java
@@ -1,5 +1,6 @@
 package com.wdelivery.member.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,10 @@ public class MemberLoginController {
 		return "main";
 	}
 	
-	
+	@PostMapping("winMemJoin.do")
+	public String winMemJoin(UserVO userVO) {
+		
+		return "";
+	}
 	
 }
